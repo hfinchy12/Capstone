de5eab8af068e413c1ae5c3c1298537c7f9b95ec
hadenj37-TAMU <hadenj37@tamu.edu>
2024-03-04 00:27:03 -0600

    analysis page progress

--- lib/src/analysis/analysis_page.dart
+++ lib/src/analysis/analysis_page.dart
@@ -1,3 +1,6 @@
+// import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AnalysisPage extends StatelessWidget {
@@ -9,16 +12,51 @@ class AnalysisPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Analysis Page'),
+        title: const Text('Analysis Results'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Analysis Page'),
-          ],
-        ),
-      ),
+      body: _MetricBar()
     );
   }
+}
+
+class _MetricBar extends StatefulWidget {
+  @override
+  MetricBarState createState() => MetricBarState();
+}
+
+class MetricBarState extends State<_MetricBar> {
+  // Based on code from https://api.flutter.dev/flutter/widgets/ListView-class.html
+  Widget _addMetrics() {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        const ListTile(
+          title: Text('Metric A: Poor', textAlign: TextAlign.left),
+        ),
+        const Divider(),
+        const ListTile(
+          title: Text('Metric B: Fair', textAlign: TextAlign.left),
+        ),
+        const Divider(),
+        const ListTile(
+          title: Text('Metric C: Good', textAlign: TextAlign.left),
+        ),
+        const Divider(),
+        Container(
+            height: 100,
+            color: Colors.grey[200],
+            child: const Text("GPT-4 Feedback", textAlign: TextAlign.left)
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Image.asset(imagePath)
+      body: _addMetrics()
+    );
+  }
+
 }
